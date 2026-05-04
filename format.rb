dd2ea0c51e55322640de61a48e7f53f0e032e32e
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2026-05-04 21:22:14 +1000

    Apply rubocop auto formatting

--- format.rb
+++ format.rb
@@ -7,85 +7,74 @@
 # $Id: format.rb,v 1.12 2008/03/25 17:25:08 francis Exp $
 
 module MySociety
-    module Format
+  module Format
+    # Word wrap the body of a text email.
+    def self.wrap_email_body(body, line_width = 69, indent = '     ')
+      body = body.gsub(/\r\n/, "\n") # forms post with \r\n by default
+      paras = body.split(/\n\n/)
 
-        # Word wrap the body of a text email.
-        def Format.wrap_email_body(body, line_width = 69, indent = "     ")
-            body = body.gsub(/\r\n/, "\n") # forms post with \r\n by default
-            paras = body.split(/\n\n/)
-
-            result = ''
-            for para in paras
-                para.gsub!(/\s+/, ' ')
-                para.gsub!(/(.{1,#{line_width}})(\s+|$)/, "#{indent}\\1\n")
-                para.strip!
-                result = result + indent + para + "\n\n"
-            end
-            return result
-        end
-
-        # Returns text with obvious links made into HTML hrefs.
-        # Taken originally from phplib/utility.php and from WordPress, tweaked somewhat.
-        def Format.make_clickable(text, params = {})
-            nofollow = params[:nofollow]
-            contract = params[:contract]
-
-            ret = ' ' + text + ' '
-            ret = ret.gsub(/(https?):\/\/([^\s<>{}()]+[^\s.,<>{}()])/i, "<a href='\\1://\\2'" + (nofollow ? " rel='nofollow'" : "") + ">\\1://\\2</a>")
-            ret = ret.gsub(/(\s)www\.([a-z0-9\-]+)((?:\.[a-z0-9\-\~]+)+)((?:\/[^ <>{}()\n\r]*[^., <>{}()\n\r])?)/i,
-                        "\\1<a href='http://www.\\2\\3\\4'" + (nofollow ? " rel='nofollow'" : "") + ">www.\\2\\3\\4</a>")
-            if contract
-                ret = ret.gsub(/(<a href='[^']*'(?: rel='nofollow')?>)([^<]{40})[^<]{3,}<\/a>/, '\\1\\2...</a>')
-            end
-            ret = ret.gsub(/(\s)([a-z0-9\-_.]+)@([^,< \n\r]*[^.,< \n\r])/i, "\\1<a href=\"mailto:\\2@\\3\">\\2@\\3</a>")
-            ret = ret.strip
-            return ret
-        end
-
-        # Simplify bracketed URLs like: www.liverpool.gov.uk <http://www.liverpool.gov.uk> 
-        # (so that the URL appears only once, and so that the escaping of the < > doesn't
-        # get &gt; contaminated into the linked URL)
-        def Format.simplify_angle_bracketed_urls(text)
-            ret = ' ' + text + ' '
-            #ret = ret.gsub(/(www\.[^\s<>{}()])\s+\<(https?):\/\//i, "\\1")
-            ret = ret.gsub(/(www\.[^\s<>{}()]+)\s+<http:\/\/\1>/i, "\\1")
-            ret = ret.strip
-            return ret
-        end
-
-        # Differs from the Rails view helper pluralize, by not including the
-        # number in the case of the singular.
-        def Format.fancy_pluralize(num, singular, plural)
-            if num == 1
-                return singular
-            else
-                return num.to_s + " " + plural
-            end
-        end
-
-        # Simplified a name to something usable in a URL
-        def Format.simplify_url_part(text, max_len = nil)
-            text = text.downcase # this also clones the string, if we use downcase! we modify the original
-            text.gsub!(/(\s|-|_)/, "_")
-            text.gsub!(/[^a-z0-9_]/, "")
-            text.gsub!(/_+/, "_")
-
-            # If required, trim down to size
-            if not max_len.nil?
-                if text.size > max_len
-                    text = text[0..(max_len-1)]
-                end
-                # removing trailing _
-                text.gsub!(/_*$/, "")
-            end
-            if text.size < 1
-                text = 'user' # just do user1, user2 etc.
-            end
-
-            text
-        end
-
+      result = ''
+      for para in paras
+        para.gsub!(/\s+/, ' ')
+        para.gsub!(/(.{1,#{line_width}})(\s+|$)/, "#{indent}\\1\n")
+        para.strip!
+        result = result + indent + para + "\n\n"
+      end
+      result
     end
+
+    # Returns text with obvious links made into HTML hrefs.
+    # Taken originally from phplib/utility.php and from WordPress, tweaked somewhat.
+    def self.make_clickable(text, params = {})
+      nofollow = params[:nofollow]
+      contract = params[:contract]
+
+      ret = ' ' + text + ' '
+      ret = ret.gsub(%r{(https?)://([^\s<>{}()]+[^\s.,<>{}()])}i,
+                     "<a href='\\1://\\2'" + (nofollow ? " rel='nofollow'" : '') + '>\\1://\\2</a>')
+      ret = ret.gsub(%r{(\s)www\.([a-z0-9-]+)((?:\.[a-z0-9\-~]+)+)((?:/[^ <>{}()\n\r]*[^., <>{}()\n\r])?)}i,
+                     "\\1<a href='http://www.\\2\\3\\4'" + (nofollow ? " rel='nofollow'" : '') + '>www.\\2\\3\\4</a>')
+      ret = ret.gsub(%r{(<a href='[^']*'(?: rel='nofollow')?>)([^<]{40})[^<]{3,}</a>}, '\\1\\2...</a>') if contract
+      ret = ret.gsub(/(\s)([a-z0-9\-_.]+)@([^,< \n\r]*[^.,< \n\r])/i, '\\1<a href="mailto:\\2@\\3">\\2@\\3</a>')
+      ret.strip
+    end
+
+    # Simplify bracketed URLs like: www.liverpool.gov.uk <http://www.liverpool.gov.uk>
+    # (so that the URL appears only once, and so that the escaping of the < > doesn't
+    # get &gt; contaminated into the linked URL)
+    def self.simplify_angle_bracketed_urls(text)
+      ret = ' ' + text + ' '
+      # ret = ret.gsub(/(www\.[^\s<>{}()])\s+\<(https?):\/\//i, "\\1")
+      ret = ret.gsub(%r{(www\.[^\s<>{}()]+)\s+<http://\1>}i, '\\1')
+      ret.strip
+    end
+
+    # Differs from the Rails view helper pluralize, by not including the
+    # number in the case of the singular.
+    def self.fancy_pluralize(num, singular, plural)
+      return singular if num == 1
+
+      num.to_s + ' ' + plural
+    end
+
+    # Simplified a name to something usable in a URL
+    def self.simplify_url_part(text, max_len = nil)
+      text = text.downcase # this also clones the string, if we use downcase! we modify the original
+      text.gsub!(/(\s|-|_)/, '_')
+      text.gsub!(/[^a-z0-9_]/, '')
+      text.gsub!(/_+/, '_')
+
+      # If required, trim down to size
+      unless max_len.nil?
+        text = text[0..(max_len - 1)] if text.size > max_len
+        # removing trailing _
+        text.gsub!(/_*$/, '')
+      end
+      if text.size < 1
+        text = 'user' # just do user1, user2 etc.
+      end
+
+      text
+    end
+  end
 end
-
-
--- validate.rb
+++ validate.rb
@@ -7,44 +7,41 @@
 # $Id: validate.rb,v 1.2 2007/12/17 18:30:59 francis Exp $
 
 module MySociety
-    module Validate
+  module Validate
+    def self.email_match_regexp
+      # This is derived from the grammar in RFC2822.
+      # mailbox = local-part "@" domain
+      # local-part = dot-string | quoted-string
+      # dot-string = atom ("." atom)*
+      # atom = atext+
+      # atext = any character other than space, specials or controls
+      # quoted-string = '"' (qtext|quoted-pair)* '"'
+      # qtext = any character other than '"', '\', or CR
+      # quoted-pair = "\" any character
+      # domain = sub-domain ("." sub-domain)* | address-literal
+      # sub-domain = [A-Za-z0-9][A-Za-z0-9-]*
+      # XXX ignore address-literal because nobody uses those...
 
-        def Validate.email_match_regexp
-            # This is derived from the grammar in RFC2822.
-            # mailbox = local-part "@" domain
-            # local-part = dot-string | quoted-string
-            # dot-string = atom ("." atom)*
-            # atom = atext+
-            # atext = any character other than space, specials or controls
-            # quoted-string = '"' (qtext|quoted-pair)* '"'
-            # qtext = any character other than '"', '\', or CR
-            # quoted-pair = "\" any character
-            # domain = sub-domain ("." sub-domain)* | address-literal
-            # sub-domain = [A-Za-z0-9][A-Za-z0-9-]*
-            # XXX ignore address-literal because nobody uses those...
+      specials = '()<>@,;:\\\\".\\[\\]'
+      controls = '\\000-\\037\\177'
+      highbit = '\\200-\\377'
+      atext = "[^#{specials} #{controls}#{highbit}]"
+      atom = "#{atext}+"
+      dot_string = "#{atom}(\\s*\\.\\s*#{atom})*"
+      qtext = "[^\"\\\\\\r\\n#{highbit}]"
+      quoted_pair = '\\.'
+      quoted_string = "\"(#{qtext}|#{quoted_pair})*\""
+      local_part = "(#{dot_string}|#{quoted_string})"
+      sub_domain = '[A-Za-z0-9][A-Za-z0-9-]*'
+      domain = "#{sub_domain}(\\s*\\.\\s*#{sub_domain})*"
 
-            specials = '()<>@,;:\\\\".\\[\\]'
-            controls = '\\000-\\037\\177'
-            highbit = '\\200-\\377'
-            atext = "[^#{specials} #{controls}#{highbit}]"
-            atom = "#{atext}+"
-            dot_string = "#{atom}(\\s*\\.\\s*#{atom})*"
-            qtext = "[^\"\\\\\\r\\n#{highbit}]"
-            quoted_pair = '\\.'
-            quoted_string = "\"(#{qtext}|#{quoted_pair})*\""
-            local_part = "(#{dot_string}|#{quoted_string})"
-            sub_domain = '[A-Za-z0-9][A-Za-z0-9-]*'
-            domain = "#{sub_domain}(\\s*\\.\\s*#{sub_domain})*"
-
-            return "#{local_part}\\s*@\\s*#{domain}"
-        end
-
-        def Validate.is_valid_email(addr)
-            is_valid_address_re = Regexp.new("^#{Validate.email_match_regexp}\$")
-
-            return addr =~ is_valid_address_re
-        end
+      "#{local_part}\\s*@\\s*#{domain}"
     end
+
+    def self.is_valid_email(addr)
+      is_valid_address_re = Regexp.new("^#{Validate.email_match_regexp}\$")
+
+      addr =~ is_valid_address_re
+    end
+  end
 end
-
-
